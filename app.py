7af8c4c1c41b0df093f71cda29a7b0af0673b277
Martin Nyaga <nyaga.martin@nobilisltd.co.ke>
2019-10-26 18:01:55 +0100

    Add empty API endpoints

--- app.py
+++ app.py
@@ -2,6 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/users", methods=["POST"])
+def new_user():
+    # Accepts the following parameters:
+    # - safe: true/false
+    # Creates user in database and returns ID
+    return 'new_user'
+
+@app.route("/users/<id>", methods=["POST"])
+def update_user():
+    return 'update user'
+    # Receives user details:
+    # - id
+    # - full_name
+    # - date_of_birth
+    # Updates the user in the database and returns success
+
+@app.route("/users")
+def users():
+    return 'list users'
+    # Returns a list of all users with all their details
+    # Unknown for users whose names are unknown
