f618a0813ad70d3e0f0c1082b9d0160343571d01
Martin Nyaga <nyaga.martin@nobilisltd.co.ke>
2019-10-26 21:44:10 +0100

    Fixed update of the safe parameter

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from app.models import User
 def new_user():
     # Accepts the following parameters:
     # - safe: true/false
-    safe = request.json['safe']
+    safe = request.json.get('safe')
 
     # Creates user in database and returns user id
     # to the frontend as json
@@ -18,17 +18,22 @@ def new_user():
 
 @app.route("/users/<id>", methods=["POST"])
 def update_user(id):
-    # Receives user details:
-    # - id
-    # Query database User object using id:
     u = User.query.get(id)
-    # - Updates full_name
-    u.full_name = request.json['full_name']
-    # - Updates date_of_birth
-    u.date_of_birth = datetime.datetime.strptime(request.json['date_of_birth'], "%d/%m/%Y").date()
+
+    full_name = request.json.get('full_name')
+    if full_name != None:
+        u.full_name = full_name
+
+    date_of_birth = request.json.get('date_of_birth')
+    if date_of_birth != None:
+        u.date_of_birth = datetime.datetime.strptime(date_of_birth, "%d/%m/%Y").date()
+
+    safe = request.json.get('safe')
+    if safe != None:
+        u.safe = safe
+
     db.session.add(u)
     db.session.commit()
-    # Updates the user in the database and returns success
     return jsonify(success=True)
 
 @app.route("/users")
